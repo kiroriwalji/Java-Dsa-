1a1cc0a3bc0fdbe38cebe757ea003156be9a0063
vishal <vishalkiroriwalji@gmail.com>
2024-04-06 18:00:08 +0530

    Linked list print using recurrsion

---
+++ src/LinkedList/ReverseLinkedlistRecurrsion.java
@@ -0,0 +1,46 @@
+package LinkedList;
+
+public class ReverseLinkedlistRecurrsion {
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+
+    public static void reverseRecurrsion(Node head) {
+        if (head == null) return;
+
+        reverseRecurrsion(head.next);
+        System.out.print(head.data + " ");
+    }
+
+    public static void displayRecurrsion(Node head) {
+        if (head == null) return;
+        System.out.print(head.data + " ");
+        displayRecurrsion(head.next);
+    }
+
+    public static void main(String[] args) {
+
+        // Making Node
+        Node a = new Node(5);
+        Node b = new Node(6);
+        Node c = new Node(7);
+        Node d = new Node(8);
+
+        //making Connection between nodes
+        a.next = b;
+        b.next = c;
+        c.next = d;
+
+        System.out.println("Recurrsion --> ");
+        displayRecurrsion(a);
+        System.out.println();
+        System.out.println("Reverse -->  ");
+        reverseRecurrsion(a);
+    }
+}
